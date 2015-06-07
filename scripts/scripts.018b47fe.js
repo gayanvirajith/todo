a72e8f3384166072b2a4b3856e985f15c47c6da3
Gayan Virajith <gayanvirajith@gmail.com>
2015-06-07 10:27:42 +0530

    Initail dist subtree commit

---
+++ scripts/scripts.018b47fe.js
@@ -0,0 +1,1 @@
+"use strict";angular.module("todoApp",["ngAnimate","ngAria","ngSanitize","ngTouch"]),angular.module("todoApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
