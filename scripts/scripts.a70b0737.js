9ac1db38f920d57c818877eade2991ee20c40a66
Gayan Virajith <gayanvirajith@gmail.com>
2015-07-28 00:10:00 +0530

    add main module and todo module

--- scripts/scripts.a70b0737.js
+++
@@ -1,1 +0,0 @@
-"use strict";angular.module("todoApp",["ngAnimate","ngAria","ngSanitize","ngTouch","ngMaterial"]),angular.module("todoApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=[{name:"HTML5 Boilerplate",done:!1},{name:"AngularJS",done:!1},{name:"Karma",done:!1}]}]);
